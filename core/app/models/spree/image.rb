a01fac4b9a2413e3f315cad83903989cfa093592
Jonathan Le <jonathan@vinova.sg>
2014-11-18 23:09:06 +0700

    Added method for multiple image type

--- core/app/models/spree/image.rb
+++ core/app/models/spree/image.rb
@@ -21,6 +21,18 @@ module Spree
       attachment.url(:mini, false)
     end
 
+    def small_url
+      attachment.url(:small, false)
+    end
+
+    def product_url
+      attachment.url(:product, false)
+    end
+
+    def large_url
+      attachment.url(:large, false)
+    end
+
     def find_dimensions
       temporary = attachment.queued_for_write[:original]
       filename = temporary.path unless temporary.nil?
